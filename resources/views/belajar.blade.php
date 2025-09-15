a89e48f3e44b270deeaeaba3c49242dacb6b67b7
Eurisko Wahyu <wahyueurisko@gmail.com>
2025-09-15 12:03:40 +0700

    menambahkan fitur bab 9

--- resources/views/belajar.blade.php
+++ resources/views/belajar.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,16 @@
     <title>Document</title>
     @vite('resources/css/app.css')
 </head>
+
 <body>
-<h1>Belajar Laravel</h1>
-@vite('resources/js/app.js')
+    <h1>Belajar Laravel</h1>
+    <div class="container text-center py-4">
+        <h1>Belajar Laravel</h1>
+        <button id="myPopover" type="button" class="btn btn-lg btn-danger mt-4" data-bs-toggle="popover"
+            title="Lagi serius..." data-bs-content="Buku Laravel Uncover top bgt!">
+            Belajar Laravel </button>
+    </div>
+    @vite('resources/sass/style.scss')
 </body>
+
 </html>
